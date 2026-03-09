57d5622b12bbe9cb4f295194e0beddb3bbb0f936
kaiser <bechagas.chagas@gmail.com>
2026-03-09 20:50:50 -0300

    Add router (Blog)

---
+++ src/blog/router.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, status
+from ..database.session import db_dependency
+
+router = APIRouter(
+    prefix='/api/blog/posts',
+    tags=['posts']
+)
+
+@router.get('', status_code=status.HTTP_200_OK)
+async def get_posts(db: db_dependency):
+    return
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+async def get_post(id: int, db: db_dependency):
+    return
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_post(db: db_dependency):
+    return
+
+@router.put('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_all_post(id: int, db: db_dependency):
+    return
+
+@router.patch('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_post(id: int, db: db_dependency):
+    return
+
+@router.get('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int, db: db_dependency):
+    return
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
+from .database.config import Base, engine
+from .blog.router import router as blog_router
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(blog_router)
